582a121f7424e989c3efdd11ba8cee186dea083b
zyy <1332616027@qq.com>
2024-04-05 22:30:23 +0800

    dp&回溯题单建立

---
+++ leetcode题解/src/0001.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> pos;
+        for(int i = 0; i < nums.size(); i ++)
+        {
+            if(pos.find(target - nums[i]) != pos.end())
+            {
+                return {i, pos[target - nums[i]]};
+            }
+            pos[nums[i]] = i;
+        }
+        return {};
+    }
+};
